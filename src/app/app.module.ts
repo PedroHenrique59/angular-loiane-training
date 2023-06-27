7c259edc188313f0237d7c9a4b4e173b151abe42
Pedro Henrique <pedro.henrique59@outlook.com>
2023-06-27 18:57:44 -0300

    Trabalhando com ngStyle.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { OutputPropertyComponent } from './output-property/output-property.compo
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.co
     OutputPropertyComponent,
     DiretivaNgifComponent,
     DiretivaNgswitchComponent,
-    DiretivaNgclassComponent
+    DiretivaNgclassComponent,
+    DiretivaNgstyleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
