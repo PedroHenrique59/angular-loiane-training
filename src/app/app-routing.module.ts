f78b14cfe5862f9eb7b5f43da62135a4b9516d6c
Pedro Henrique <pedro.henrique59@outlook.com>
2023-07-04 15:50:58 -0300

    Trabalhando com rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './componentes-rotas/home/home.component';
+import {LoginComponent} from './componentes-rotas/login/login.component';
+import {CursosComponent} from './componentes-rotas/cursos/cursos.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cursos', component: CursosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
