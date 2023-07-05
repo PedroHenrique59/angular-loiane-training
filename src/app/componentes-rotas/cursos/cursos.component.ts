b83e41a3812c4dba8c420e437c5b93340582eee3
Pedro Henrique <pedro.henrique59@outlook.com>
2023-07-05 14:09:44 -0300

    Trabalhando com rotas com query parameters.

--- src/app/componentes-rotas/cursos/cursos.component.ts
+++ src/app/componentes-rotas/cursos/cursos.component.ts
@@ -1,20 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CursosService} from '../../services/cursos.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent implements OnInit, OnDestroy {
 
   cursos: any[];
 
-  constructor(private cursoService: CursosService) {
+  pagina: number;
+
+  inscricao: Subscription;
+
+  constructor(private cursoService: CursosService, private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit() {
     this.cursos = this.cursoService.getCursos();
+    this.inscricao = this.route.queryParams.subscribe(parametro => {
+      this.pagina = parametro.pagina;
+    });
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina() {
+    this.pagina++;
+    this.router.navigate(['/cursos'], {queryParams: {'pagina': this.pagina}});
   }
 
 }
